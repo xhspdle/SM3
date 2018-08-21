ef4d6583116380a214f1c6bc25a927323ef74931
JHTA <JHTA@JHTA-PC>
2018-08-21 11:37:21 +0900

    dddd

--- 00semi_project/src/sm3/lsu/test/testboard.java
+++ 00semi_project/src/sm3/lsu/test/testboard.java
@@ -3,6 +3,6 @@ package sm3.lsu.test;
 public class testboard {
 	public static void main(String[] args) {
 		System.out.println("dfgdg");
-		System.out.println("dfgdg");
+		System.out.println("dfgd");
 	}
 }
